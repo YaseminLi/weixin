35e5d5705758a494e04d7f7f3872171d2e0acd06
YaseminLi <yaseminli@163.com>
2019-04-26 23:51:59 +0800

    获取数据

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 App({
 globalData:{
   // g_isPlayingMusic:false,
-  g_currentMusicPostId:null
+  g_currentMusicPostId:null,
+  doubanbase:'http://t.yushu.im'
 },
 onLaunch:function(){
     wx.setStorageSync('isPlayingMusic', false)
--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,78 +1,52 @@
-// pages/movie/movie.js
+var app = getApp();
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  onLoad: function(options) {
+    var inTheatersUrl = app.globalData.doubanbase + '/v2/movie/in_theaters' + '?start=0&count=3';
+    var comingSoonUrl = app.globalData.doubanbase + '/v2/movie/coming_soon' + '?start=0&count=3';
+    var top250Url = app.globalData.doubanbase + '/v2/movie/top250' + '?start=0&count=3';
+    //是异步调用的
+    this.getMovieListData(inTheatersUrl);
+    // this.getMovieListData(comingSoonUrl);
+    // this.getMovieListData(top250Url);
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  getMovieListData: function(url) {
+    var that = this;
     wx.request({
-      url: 'http://t.yushu.im/v2/movie/top250',
-      method:'GET',
-      header:{
-        "content-type":"application/json"
+      url: url,
+      method: 'GET',
+      header: {
+        "content-type": "application/json"
       },
-      success:function(res){
-        // console.log(res)
+      success: function(res) {
+        // console.log(res);
+        that.processDoubanData(res.data)
       },
-      fail:function(){
-        console.log('fail')
+      fail: function(error) {
+        console.log(error)
       }
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  processDoubanData: function(moviesDouban) {
+    var movies = [];
+    var subjects = moviesDouban.subjects;
+    for (var idx in subjects) {
+      var title = subjects[idx].title;
+      if (title.length > 6) {
+        title = title.substring(0, 6) + '...';
+      }
+      var temp = {
+        coverageUrl: subjects[idx].images.large,
+        title: title,
+        movieId: subjects[idx].id,
+        average: subjects[idx].rating.average
+      }
+      movies.push(temp);
+    }
+    this.setData({
+      movies: movies
+    })
+    console.log(this.data.movies)
   }
+
 })
